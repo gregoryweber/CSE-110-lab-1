f01bd354c152d5def8c161e8131cddb337598a5f
gregoryweber <gregory1@weberdao.org>
2024-09-30 14:34:47 -0700

    Task 5 done

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  document.getElementById("subscribe-form").addEventListener("submit", alertSubFunc);
+}
+
+function alertSubFunc() {
+  alert(`Subscriber "added": ${document.getElementById("email").value}`);
+}
+  
